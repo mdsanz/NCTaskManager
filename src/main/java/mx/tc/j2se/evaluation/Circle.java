1407db8d97a22c6f8ab2671287cc016b7864e825
Marcos Sanchez <marcos.sanzlara@gmail.com>
2022-06-29 18:38:11 -0500

    Adding comments to evaluation 1

--- src/main/java/mx/tc/j2se/evaluation/Circle.java
+++ src/main/java/mx/tc/j2se/evaluation/Circle.java
@@ -1,12 +1,23 @@
 package mx.tc.j2se.evaluation;
 
 public class Circle {
+    /**
+     * It's literally the radius of the circle.
+     */
     private int radius;
 
+    /**
+     * Constructs a circle with radius 1.
+     */
     public Circle() {
         this.radius = 1;
     }
 
+    /**
+     * Constructs a circle with the given radius
+     * @param radius It's the radius of the circle.
+     * @throws IllegalArgumentException The radius of the circle cannot be negative or zero.
+     */
     public Circle(int radius) throws IllegalArgumentException {
         if (radius <= 0) {
             throw new IllegalArgumentException("The radius is invalid");
@@ -15,6 +26,11 @@ public class Circle {
         }
     }
 
+    /**
+     * Method to modify the radius of a circle.
+     * @param radius It's the new radius of the circle.
+     * @throws IllegalArgumentException The radius of the circle cannot be negative or zero.
+     */
     public void setRadius(int radius) throws IllegalArgumentException {
         if (radius <= 0) {
             throw new IllegalArgumentException("The radius is invalid");
@@ -23,10 +39,18 @@ public class Circle {
         }
     }
 
+    /**
+     * Method to shows the radius of a circle.
+     * @return the radius of the circle.
+     */
     public int getRadius() {
         return radius;
     }
 
+    /**
+     * Method to shows the area of a circle.
+     * @return the area of the circle.
+     */
     public double getArea() {
         return Math.PI * Math.pow(radius, 2);
     }
--- src/main/java/mx/tc/j2se/evaluation/Evaluation1.java
+++ src/main/java/mx/tc/j2se/evaluation/Evaluation1.java
@@ -1,6 +1,11 @@
 package mx.tc.j2se.evaluation;
 
 public class Evaluation1 {
+    /**
+     * Static method to know the index of the largest circle in a Circle array.
+     * @param arrayCircle It's an array which contains Circle objects.
+     * @return the index of the largest circle.
+     */
     public static int biggestCircle(Circle[] arrayCircle) {
         int index = -1, r = -1;
         for (int i = 0; i < arrayCircle.length; i++) {
@@ -14,6 +19,7 @@ public class Evaluation1 {
 
     public static void main(String[] args) {
         System.out.println("Evaluation 1");
+        System.out.println("============");
         try {
             Circle circle1 = new Circle(0);
         } catch (IllegalArgumentException err) {
